c11e7385a2d09bdb38d75ac9ae816e50d6c65c15
MOUHCINE <MouhcineOualyali@gmail.com>
2023-12-19 18:40:55 +0100

    test some functionalty od project feature

--- apps/backend/urls.py
+++ apps/backend/urls.py
@@ -1,7 +1,7 @@
 
 from django.urls import path
 
-from .views.projects import ProjectListView, ProjectUpdateView, ProjectCreateView, ProjectDeleteView
+from .views.projects import ProjectListView, ProjectUpdateView, ProjectCreateView, ProjectDeleteView, ProjectDetailView
 from .views.views import index
 
 urlpatterns = [
@@ -10,4 +10,5 @@ urlpatterns = [
     path('projects/create/', view=ProjectCreateView.as_view(), name='project-create'),
     path('projects/<int:pk>/update/', view=ProjectUpdateView.as_view(), name='project-update'),
     path('projects/<int:pk>/delete/', view=ProjectDeleteView.as_view(), name='project-delete'),
+    path('projects/<int:pk>/detail/', view=ProjectDetailView.as_view(), name='project-detail'),
 ]
--- apps/backend/views/projects.py
+++ apps/backend/views/projects.py
@@ -1,4 +1,5 @@
-from django.views.generic import ListView, CreateView, UpdateView, DeleteView
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView, DetailView
+from django.urls import reverse_lazy
 
 from ..models import Project
 
@@ -10,12 +11,19 @@ class ProjectCreateView(CreateView):
     model           =   Project
     template_name   =   'projects/project_create.html'
     fields          =   '__all__'
+    success_url     =   reverse_lazy('project-list')
     
 class ProjectUpdateView(UpdateView):
     model           =   Project
     template_name   =   'projects/project_create.html'
     fields          =   '__all__'
+    success_url     =   reverse_lazy('project-list')
 
 class ProjectDeleteView(DeleteView):
     model           =   Project
     template_name   =   'projects/project_delete.html'
+    success_url     =   reverse_lazy('project-list')
+    
+class ProjectDetailView(DetailView):
+    model           =   Project
+    template_name   =   'projects/project_detail.html'
